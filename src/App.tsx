0cc5f24674fab40ed86c6923abb5a0c404722df4
Benjamin Sepulveda <benjamin.sepulveda@witi.cl>
2021-11-09 10:52:20 -0300

    MUI library added. tabs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NotFound from './workshop/components/NotFound';
-import HeroList from './workshop/components/HeroList';
-import VillainList from './workshop/components/VillainList';
+import TabPanel from './workshop/components/TabPanel';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
     <div className="App">
-      <HeroList />
-      <VillainList />
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="md">
+          <TabPanel />
+        </Container>
+      </React.Fragment>
     </div>
   );
 }
